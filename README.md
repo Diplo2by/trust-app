23b582842d34c0ad869906f8e2717087984a8663
Diplo2by <contactdarshanv@gmail.com>
2022-12-24 02:58:55 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Trust app (W.I.P)
+
+A rust based checksum verification app
---
+++ frontend/pages/index.js
@@ -0,0 +1,11 @@
+import Head from "next/head"
+export default function Home() {
+  return (
+  <div>
+      <Head>
+      <title>Trust app</title>
+      </Head>
+    <h1>Trust - verify checksum and file integrity!</h1>
+    </div>
+  )
+}
