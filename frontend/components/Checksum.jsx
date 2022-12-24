6ee987ee73b70feb02e3510263c624ddf1e0b9bc
Diplo2by <contactdarshanv@gmail.com>
2022-12-24 15:20:33 +0530

    bug fixes
    changed class attribute

--- frontend/components/Checksum.jsx
+++ frontend/components/Checksum.jsx
@@ -4,7 +4,7 @@ const Checksum = () => {
   return (
     <div>
         <div>
-        <input type="text" id="default-input" class="font-mono text-l w-full mx-auto bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+        <input type="text" id="default-input" className="font-mono text-l w-full mx-auto bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
         </div>
     </div>
   )
